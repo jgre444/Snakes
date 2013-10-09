757ee7d803259d07977c6047449cdb4c9de1a8a8
Awais <awaisnisar12@gmail.com>
2013-10-09 19:27:12 +1100

    Updated menu with top scores working by Welly

--- SnakeV3/src/com/example/Snake/HighScores.java
+++ SnakeV3/src/com/example/Snake/HighScores.java
@@ -1,5 +1,11 @@
 package com.example.Snake;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import com.example.sample.R;
 
 import android.app.Activity;
@@ -8,16 +14,60 @@ import android.widget.TextView;
 
 public class HighScores  extends Activity{
 	private TextView textview;
+	private ArrayList<String> topScores;
+	private StringBuffer scoresAsString;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.highscores);
+		scoresAsString = new StringBuffer();
 		textview=(TextView)findViewById(R.id.textView1);
+		topScores = readScores();
 		getHighScores();
 	}
-	
+
 	public void getHighScores(){
-		textview.setText("Hello");
+
 		
+
+		if(topScores.size() == 0){
+			scoresAsString.append("\nreading ERROR");
+		}
+		else{
+			scoresAsString.append("Name\tScore\n\n");
+			for(int i = 0; i < topScores.size(); i++){
+				scoresAsString.append(topScores.get(i) + "\n");
+			}
+		}
+		
+
+		textview.setText(scoresAsString.toString());
+
+	}
+
+	public ArrayList<String>  readScores() {
+		ArrayList<String> scores = new ArrayList<String>();
+		
+
+		try{
+			BufferedReader newBufferReader=new BufferedReader(new 
+		            InputStreamReader(getAssets().open("Scores.txt")));
+			
+			String line = newBufferReader.readLine();
+			
+	        while (line != null) {	         
+					scores.add(line);	
+					line = newBufferReader.readLine();
+			}
+	        
+			newBufferReader.close();
+
+		} catch (Exception e) {
+			scoresAsString.append("\n" + e.getMessage());
+		}
+
+
+		return scores;
+
 	}
 }
